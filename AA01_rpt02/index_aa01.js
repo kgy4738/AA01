41a765e09aee86b8d6df533d74539752eabb9f30
kgy4738 <54879697+kgy4738@users.noreply.github.com>
2019-09-11 17:06:36 +0900

    Add files via upload

---
+++ AA01_rpt02/index_aa01.js
@@ -0,0 +1,8 @@
+// index_aa01,js
+var myinfo = require('./index_aa01info.js');
+
+myinfo("aa01", "Kimkungyoung", '010-3619-4738');
+
+myinfo("aa02", "dongjin", '010-1111-2222');
+
+myinfo("aa77", "mingu", '010-3333-4444');
