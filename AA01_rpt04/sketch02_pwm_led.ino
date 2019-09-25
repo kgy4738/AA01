8f75f5ce4c55c0140b8bd3952be64d41c0fef097
kgy4738 <54879697+kgy4738@users.noreply.github.com>
2019-09-25 16:32:45 +0900

    Add files via upload

---
+++ AA01_rpt04/sketch02_pwm_led.ino
@@ -0,0 +1,17 @@
+int pwm = 0;
+int led = 3;
+
+void setup() {
+}
+
+void loop() {
+
+  pwm = random(0,255);
+  pwmLed(led, pwm);
+}
+
+void pwmLed(int led, int pwmValue) {
+  // put your main code here, to run repeatedly:
+  analogWrite(led, pwmValue);
+  delay(10);
+}
---
+++ AA01_rpt04/sketch03_pwm_led_serial.ino
@@ -0,0 +1,21 @@
+int pwm = 0;
+int led = 3;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  pwm = random(0,255);
+  pwmLed(led, pwm);
+
+  Serial.print("AA01, LED: ");
+  Serial.println(pwm);
+  delay(10);
+}
+
+void pwmLed(int led, int pwmValue) {
+  // put your main code here, to run repeatedly:
+  analogWrite(led, pwmValue);
+  delay(10);
+}
